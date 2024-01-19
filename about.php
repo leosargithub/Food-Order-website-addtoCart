ea449a6c6efdda325952bff446d41609549f38e9
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2024-01-19 08:21:20 +0545

    first commit

---
+++ about.php
@@ -0,0 +1,222 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<div class="heading">
+   <h3>about us</h3>
+   <p><a href="home.php" >home</a> <span> / about</span></p>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>
+         for a seamless food ordering experience with a user-friendly interface, efficient order management, secure transactions, real-time updates, customization options, integration capabilities, responsive customer support, marketing tools, scalability, and a commitment to continuous improvement.
+
+
+         </p>
+         <a href="menu.php" class="btn">our menu</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="title">simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>choose order</h3>
+         <p>
+Choose your items, customize if needed, add to cart, review, proceed to checkout, make payment, receive confirmation.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-2.png" alt="">
+         <h3>fast delivery</h3>
+         <p>Fast delivery ensures your order reaches you promptly, minimizing wait times and providing a swift and convenient service.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>enjoy food</h3>
+         <p>
+"Enjoying food is savoring a symphony of flavors that delight the senses and create a pleasurable culinary experience."</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="title">customer's reivews</h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <img src="images/saroj.jpg" alt="">
+            <p>"I had an amazing experience with this food delivery! The dishes were incredibly tasty, and the delivery was surprisingly fast. I was delighted by the quick service and delicious flavors – it made my day!"</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Saroj Budhathoki</h3>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/imroj.jpg" alt="">
+            <p>"I was pleasantly surprised by the variety of options available, and the delivery was impressively quick. The food was not only fresh but also packed with flavor. This has become my preferred choice for a quick and satisfying meal!"</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Imroj  Shrestha </h3>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pratik.jpg" alt="">
+            <p>"The food was not only delicious but also arrived promptly. I appreciated the ease of customizing my order, and the overall process was hassle-free. I'm consistently impressed with the quality and speed – definitely recommending it to others!"</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Pratik bhandari</h3>
+         </div>
+
+        
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->=
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor: true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+      slidesPerView: 1,
+      },
+      700: {
+      slidesPerView: 2,
+      },
+      1024: {
+      slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="budhathokisaroj827@gmail.com">budhathokisaroj827@gmail.com</a>
+        
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">kathmandu, nepal - 44613</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:01-5134140">01-5134140</a>
+      
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y-M'); ?> by <span>Saroj Budhathoki</span> </div>
+
+</footer>
+
+
